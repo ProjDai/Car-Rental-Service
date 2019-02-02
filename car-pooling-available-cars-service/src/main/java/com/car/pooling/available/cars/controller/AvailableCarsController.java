e391668c190628f3b7325c40c4d37f97bb947892
JonathanGeb <Jonathan.gebrehewet@web.de>
2019-02-02 20:56:47 +0100

    commit

--- car-pooling-available-cars-service/src/main/java/com/car/pooling/available/cars/controller/AvailableCarsController.java
+++ car-pooling-available-cars-service/src/main/java/com/car/pooling/available/cars/controller/AvailableCarsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.car.pooling.available.cars.config.ApplicationProperties;
 
 /**
- * Die Class "AvailableCarsController".
+ * Die Class AvailableCarsController.
  */
 @RestController
 public class AvailableCarsController {
